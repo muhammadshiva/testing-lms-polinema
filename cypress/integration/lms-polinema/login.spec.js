05d566dbdd16e47b6c157119a50fc85e251dcb07
muhammadshiva <shivamuhammad07@gmail.com>
2021-12-21 17:50:42 +0700

    init project

---
+++ cypress/integration/lms-polinema/login.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Login Scenario', () => {
+
+    describe('Login Page - Possible Login Failed', () => {
+
+        beforeEach(() => {
+            cy.visit('http://lms.polinema.ac.id/')
+        })
+    })
+})
